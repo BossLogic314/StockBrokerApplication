8ba46ade2dbfe7f8e43424f8965969bbac175120
BossLogic314 <bhagavatulaanish@gmail.com>
2024-06-09 03:24:23 +0530

    Indentation

--- watchlist_manager_service/controllers/watchlists.js
+++ watchlist_manager_service/controllers/watchlists.js
@@ -4,28 +4,28 @@ import watchListsModel from '../models/WatchLists.js';
 
 export const getWatchLists = (async (req, res) => {
 
-    let accessToken = null;
-    try {
-      const jwtToken = req.cookies.jwt;
-      accessToken = verifyJwtToken(jwtToken);
-  
-      if (!accessToken) {
-        res.status(401).json({message: "User unauthorized!"});
-        return;
-      }
-    }
-    catch(error) {
+  let accessToken = null;
+  try {
+    const jwtToken = req.cookies.jwt;
+    accessToken = verifyJwtToken(jwtToken);
+
+    if (!accessToken) {
       res.status(401).json({message: "User unauthorized!"});
       return;
     }
+  }
+  catch(error) {
+    res.status(401).json({message: "User unauthorized!"});
+    return;
+  }
 
-    try {
-      const userId = req.query.userId;
-      const watchLists = await watchListsModel.findOne({userId: userId});
+  try {
+    const userId = req.query.userId;
+    const watchLists = await watchListsModel.findOne({userId: userId});
 
-      res.status(200).json({watchLists: watchLists});
-    }
-    catch(error) {
-      res.status(500).json({message: error.message});
-    }
+    res.status(200).json({watchLists: watchLists});
+  }
+  catch(error) {
+    res.status(500).json({message: error.message});
+  }
 });
