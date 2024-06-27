33e5d6476e14419baaddc58ebcd1348e495dcad2
BossLogic314 <bhagavatulaanish@gmail.com>
2024-06-28 00:45:14 +0530

    Added controller method to fetch funds and margin

--- user_manager_service/controllers/user.js
+++ user_manager_service/controllers/user.js
@@ -109,6 +109,38 @@ export const getHoldings = (async (req, res) => {
   
   axios.get(url, { headers })
     .then(response => {
+      res.status(200).json({holdings: response.data});
+    })
+    .catch(error => {
+      res.status(500).json({message: "Server error!"});
+    });
+});
+
+export const getFundsAndMargin = (async (req, res) => {
+
+  let accessToken = null;
+  try {
+    const jwtToken = req.cookies.jwt;
+    accessToken = verifyJwtToken(jwtToken);
+
+    if (!accessToken) {
+      res.status(401).json({message: "User unauthorized!"});
+      return;
+    }
+  }
+  catch(error) {
+    res.status(401).json({message: "User unauthorized!"});
+    return;
+  }
+
+  const url = 'https://api.upstox.com/v2/user/get-funds-and-margin';
+  const headers = {
+    'Accept': 'application/json',
+    'Authorization': `Bearer ${accessToken}`,
+  };
+  
+  axios.get(url, { headers })
+    .then(response => {
       res.status(200).json({orders: response.data});
     })
     .catch(error => {
--- user_manager_service/routes/user.js
+++ user_manager_service/routes/user.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { getUserInformation, getAccessToken, getOrderBook, getHoldings } from '../controllers/user.js';
+import { getUserInformation, getAccessToken, getOrderBook, getHoldings, getFundsAndMargin } from '../controllers/user.js';
 
 const router = express.Router();
 const jsonParser = bodyParser.json();
@@ -9,5 +9,6 @@ router.get('/getUserInformation', jsonParser, getUserInformation);
 router.get('/getAccessToken', jsonParser, getAccessToken);
 router.get('/getOrderBook', jsonParser, getOrderBook);
 router.get('/getHoldings', jsonParser, getHoldings);
+router.get('/getFundsAndMargin', jsonParser, getFundsAndMargin);
 
 export default router;
