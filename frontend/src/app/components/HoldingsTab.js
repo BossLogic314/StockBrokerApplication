be6155f21762bd1c715fb7965120bb7008299a6b
BossLogic314 <bhagavatulaanish@gmail.com>
2024-06-28 21:50:51 +0530

    Displaying sample data in holdings tab and made minor changes to UI

--- frontend/src/app/components/HoldingsTab.js
+++ frontend/src/app/components/HoldingsTab.js
@@ -8,7 +8,14 @@ export default function HoldingsTab() {
 
     const [holdings, setHoldings] = useState([]);
     const holdingsHeaderFields = ['Symbol', 'Net Qty', 'Avg. Price', 'LTP', 'Current Value', 'Overall P&L', 'Overall %'];
-    const [liveMarketData, setLiveMarketData] = useState([]);
+    const [liveMarketData, setLiveMarketData] = useState({
+        'NSE_EQ|INE645W01026': {
+            ltp: 31.07
+        },
+        'NSE_EQ|INE669E01016': {
+            ltp: 17.89
+        }
+    });
     const [socket, setSocket] = useState(null);
 
     const getHoldings = async () => {
@@ -25,60 +32,14 @@ export default function HoldingsTab() {
         }
     }
 
-    const getLiveMarketData = async () => {
-
-        // If no holdings (yet)
-        if (holdings.length == 0) {
-            return;
-        }
-        const newSocket = io('http://localhost:8086/');
-
-        newSocket.on('market data', (updatedLiveMarketData) => {
-
-            updatedLiveMarketData = JSON.parse(updatedLiveMarketData);
-            let liveMarketDataCopy = [...liveMarketData];
-
-            for (let i = 0; i < updatedLiveMarketData.length; ++i) {
-                liveMarketDataCopy[updatedLiveMarketData[i].instrumentKey] = updatedLiveMarketData[i];
-            }
-            setLiveMarketData(liveMarketDataCopy);
-        });
-
-        setSocket(newSocket);
-
-        let accessToken = null;
-        // Getting the access token
-        try {
-            const response = await axios.get('http://localhost:8088/user/getAccessToken',
-            {
-                withCredentials: true
-            });
-            accessToken = response.data.accessToken;
-        }
-        // The user has to login again
-        catch(error) {
-            signOut();
-        }
-
-        const instrumentKeys = holdings.map(element => element.instrument_token);
-        newSocket.emit('market data',
-        {
-            accessToken: accessToken,
-            instrumentKeys: instrumentKeys
-        });
-    }
-
     useEffect(() => {
         getHoldings();
     }, []);
 
-    useEffect(() => {
-        getLiveMarketData();
-    }, [holdings]);
-
+    console.log(holdings);
     return (
-        <div className="holdingsTab flex flex-col flex-grow my-[20px] mx-[20px] overflow-y-hidden overflow-x-hidden" id="ordersTab">
-            <div className="holdingsHeader h-[50px] flex flex-row" id="ordersHeader">
+        <div className="holdingsTab flex flex-col flex-grow my-[20px] mx-[20px] overflow-y-hidden overflow-x-hidden" id="holdingsTab">
+            <div className="holdingsHeader h-[50px] flex flex-row" id="holdingsHeader">
                 {
                     holdingsHeaderFields.map((element, index) => (
                         <div className="holdingsHeaderField w-[20%] min-w-[120px] text-[16px] font-[500] flex justify-center items-center" key={index}>
@@ -88,10 +49,10 @@ export default function HoldingsTab() {
                 }
             </div>
 
-            <div className="holdings flex-grow overflow-y-auto overflow-x-hidden" id="orders">
+            <div className="holdings flex-grow overflow-y-auto overflow-x-hidden" id="holdings">
                 {
                     holdings.map((element, index) => (
-                        <div className="holding h-[65px] flex flex-row" key={index} id="order">
+                        <div className="holding h-[65px] flex flex-row" key={index} id="holding">
                             <div className="symbolEntry flex flex-col justify-center items-center h-full w-[20%] min-w-[120px]">
                                 <div className="symbolNameEntry text-[15px] font-[500] truncate ...">{element.tradingsymbol}</div>
                                 <div className="symbolExchangeEntry text-[11px] font-[360] truncate ...">{`${element.exchange}_INDEX`}</div>
@@ -99,14 +60,35 @@ export default function HoldingsTab() {
                             <div className="netQuantityEntry flex flex-col justify-center items-center h-full w-[20%] min-w-[120px] truncate ...">
                                 {element.quantity}
                             </div>
-                            <div className="averagePriceEntry flex flex-col justify-center items-center h-full w-[20%] min-w-[120px] truncate ...">
+                            <div className="averagePriceEntry flex flex-row justify-center items-center h-full w-[20%] min-w-[120px] truncate ...">
+                                <span className="rupeeSign mr-[2px]">&#8377;</span>
                                 {element.average_price}
                             </div>
-                            <div className="ltpEntry flex flex-col justify-center items-center h-full w-[20%] min-w-[120px] truncate ...">
-                                {"ltp"}
+                            <div className="ltpEntry flex flex-row justify-center items-center h-full w-[20%] min-w-[120px] truncate ...">
+                                {liveMarketData[element.instrument_token] == null ? "" : <span className="rupeeSign mr-[2px]">&#8377;</span>}
+                                {liveMarketData[element.instrument_token] == null ? "" : liveMarketData[element.instrument_token].ltp}
                             </div>
-                            <div className="averagePriceEntry flex flex-col justify-center items-center h-full w-[20%] min-w-[120px] truncate ...">
-                                {element.average_price}
+                            <div className="currentValueEntry flex flex-row justify-center items-center h-full w-[20%] min-w-[120px] truncate ...">
+                                {liveMarketData[element.instrument_token] == null ? "" : <span className="rupeeSign mr-[2px]">&#8377;</span>}
+                                {
+                                    liveMarketData[element.instrument_token] == null ? "" :
+                                    element.quantity * liveMarketData[element.instrument_token].ltp
+                                }
+                            </div>
+                            <div className="overallProfitLossEntry flex flex-row justify-center items-center h-full w-[20%] min-w-[120px] truncate ...">
+                                {liveMarketData[element.instrument_token] == null ? "" : <span className="rupeeSign mr-[2px]">&#8377;</span>}
+                                {
+                                    liveMarketData[element.instrument_token] == null ? "" :
+                                    ((((liveMarketData[element.instrument_token].ltp - element.average_price) * element.quantity) * 100) / 100).toFixed(2)
+                                }
+                            </div>
+                            <div className="overallPercentageEntry flex flex-col justify-center items-center h-full w-[20%] min-w-[120px] truncate ...">
+                                {
+                                    liveMarketData[element.instrument_token] == null ? "" :
+                                    (((
+                                        (liveMarketData[element.instrument_token].ltp - element.average_price)
+                                        / element.average_price * element.quantity * 100) * 100) / 100).toFixed(2) + "%"
+                                }
                             </div>
                         </div>
                     ))
